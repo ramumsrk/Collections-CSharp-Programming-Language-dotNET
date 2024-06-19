6534b42538aaba5ea83c17133a0016f3ef46a662
Ramu Mohan Srinivasan Radhakrishnan <ramumsrk02071981@gmail.com>
2024-06-19 11:19:30 +0530

    Collections In .NET With C# Example
    https://shahedbd.medium.com/collections-in-net-with-c-example-c97bc027c886
    
    On branch main-Collections-C#-Programming-Language-dotNET
    
    Initial commit
    
    Changes to be committed:
    new file:   .gitignore
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/00-Introduction/00-Introduction.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/01-System.Collections.Generic.List/01-System.Collections.Generic.List.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/02-System.Collection.Generic.Dictionary/02-System.Collections.Generic.Dictionary.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/03-System.Collections.Generic.HashSet/03-System.Collections.Generic.HashSet.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/04-System.Collections.Generic.Queue/04-System.Collections.Generic.Queue.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/05-System.Collections.Generic.Stack/05-System.Collections.Generic.Stack.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/dotnet_build_--verbosity_detailed.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/dotnet_new_create_console_--name_--output_--framework_net8.0_--language_C#_--langVersion_12_--verbosity_detailed.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/dotnet_new_create_solution_--name_--output_--verbosity_detailed.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/dotnet_run_--verbosity_detailed_--project.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/github.com/shahedbd/shahedbd.txt
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/src/project/Collections-In-dotNET-With-CSharp-Example/DemoCollections/DemoCollections.csproj
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/src/project/Collections-In-dotNET-With-CSharp-Example/DemoCollections/Program.cs
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/src/solution/Collections-In-dotNET-With-CSharp-Example/collections_in_dotNET_with_CSharp_example.sln
    new file:   shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/www.youtube.com/@DotNetTech/DotNetTech.txt

---
+++ shahedbd.medium.com/collections-in-net-with-c-example/Collections-In-dotNET-With-CSharp-Example/src/project/Collections-In-dotNET-With-CSharp-Example/DemoCollections/Program.cs
@@ -0,0 +1,65 @@
+﻿System.Collections.Generic.List<int> _listOfNumbers = [
+  1,
+  2,
+  3,
+];
+
+foreach (System.Int32 _number in _listOfNumbers)
+{
+  System.Console.WriteLine(
+    $"{Environment.NewLine}Number is: {_number}"
+  );
+}
+
+System.Collections.Generic.Dictionary<int,string> _serialNumberToSubjects = new System.Collections.Generic.Dictionary<int,string>() {
+  {1, "EEE"},
+  {2, "CSE"},
+  {3, "ICT"},
+  {4, "ECE"},
+};
+
+foreach (var _serialNumberToSubject in _serialNumberToSubjects)
+{
+  System.Console.WriteLine(
+    $"{Environment.NewLine}Key: {_serialNumberToSubject.Key} and Value: {_serialNumberToSubject.Value}"
+  );
+}
+
+System.Collections.Generic.HashSet<int> _hashSetOfNumbers = [
+  1,
+  2,
+  3,
+  3,
+];
+foreach (var _number in _hashSetOfNumbers)
+{
+  System.Console.WriteLine(
+    $"{Environment.NewLine}Number: {_number}"
+  );
+}
+
+System.Collections.Generic.Queue<string> _stringsQueue = new System.Collections.Generic.Queue<string>();
+
+_stringsQueue.Enqueue("first");
+_stringsQueue.Enqueue("second");
+_stringsQueue.Enqueue("third");
+
+while (_stringsQueue.Count > 0)
+{
+  System.Console.WriteLine(
+    $"{Environment.NewLine}Element: '{_stringsQueue.Dequeue()}'"
+  );
+}
+
+System.Collections.Generic.Stack<string> _stringsStack = new System.Collections.Generic.Stack<string>();
+
+_stringsStack.Push("First");
+_stringsStack.Push("Second");
+_stringsStack.Push("Third");
+
+while (_stringsStack.Count > 0)
+{
+  System.Console.WriteLine(
+    $"{Environment.NewLine}Element: {_stringsStack.Pop()}"
+  );
+}
